6c32dedc10cb57bcd47c98afdc40dcba9135cada
Shawn Place <shawnplace11@gmail.com>
2020-01-14 19:58:02 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace JSConsoleTemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInst = new System.Windows.Forms.Label();
+            this.btnMain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInst
+            // 
+            this.lblInst.AutoSize = true;
+            this.lblInst.Location = new System.Drawing.Point(35, 28);
+            this.lblInst.Name = "lblInst";
+            this.lblInst.Size = new System.Drawing.Size(322, 17);
+            this.lblInst.TabIndex = 0;
+            this.lblInst.Text = "Push Button to Clear/Create JS Console Template";
+            // 
+            // btnMain
+            // 
+            this.btnMain.Location = new System.Drawing.Point(121, 77);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(132, 32);
+            this.btnMain.TabIndex = 1;
+            this.btnMain.Text = "Run";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.BtnMain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 136);
+            this.Controls.Add(this.btnMain);
+            this.Controls.Add(this.lblInst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInst;
+        private System.Windows.Forms.Button btnMain;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JSConsoleTemplate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMain_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Creates HTML Template File w/ button and linked 'test' script
+                StreamWriter templateFile;
+
+                templateFile = File.CreateText("index.html");
+
+                string templateCode =
+                    "<!DOCTYPE html>\r\n" +
+                    "<html lang='en'>\r\n" +
+                    "<head>\r\n" +
+                    "   <meta charset='UTF-8'>\r\n" +
+                    "   <meta name='viewport' content='width=device-width, initial-scale=1.0'>\r\n" +
+                    "   <meta http-equiv='X-UA-Compatible' content='ie=edge'>\r\n" +
+                    "   <title>Document</title>\r\n" +
+                    "</head>\r\n" +
+                    "<body>\r\n" +
+                    "   <h1>Make sure console is open</h1>\r\n" +
+                    "   <button onClick='showInConsole()'>Click Here</button>\r\n" +
+                    "   <script src='test.js'></script>\r\n" +
+                    "</body>\r\n" +
+                    "</html>";
+
+                templateFile.WriteLine(templateCode);
+
+                templateFile.Close();
+
+                // Creates JS File with linked function and script name
+                StreamWriter scriptFile;
+
+                scriptFile = File.CreateText("test.js");
+
+                string scriptCode =
+                    "function showInConsole() {\r\n" +
+                    "   console.log('Test');\r\n" +
+                    "}";
+
+                scriptFile.WriteLine(scriptCode);
+
+                scriptFile.Close();
+
+                System.Diagnostics.Process.Start("index.html");
+                System.Diagnostics.Process.Start("test.js");
+
+                Application.Exit();
+            }
+            catch (Exception catchException)
+            {
+                MessageBox.Show(catchException.Message);
+            }
+        }
+    }
+}
